24b98aee03f445f0f01da340109cd5f80d3bbdb2
Ankiit Maisuria <ankiit.maisuria33@gmail.com>
2026-05-31 15:42:39 -0700

    DataTypes in Python

---
+++ 02_data_types/mini_project_03.py
@@ -0,0 +1,7 @@
+#Generate random numbers
+"""Python doesn't have random() funtion to make random numbers, but Python has a buil-in module
+called random that can generate random numbers"""
+
+import random
+
+print(random.randrange(1, 100))
---
+++ 02_data_types/mini_project_04.py
@@ -0,0 +1,8 @@
+a = 10
+b = 3
+
+print(a/b)
+print(round(a/b,2))
+print(f"{a/b:.2f}")
+print(a//b)
+print(a%b)
