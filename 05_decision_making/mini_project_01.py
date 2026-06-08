ea3478fd3e011ed777374d997f1508b6207f2097
Ankiit Maisuria <ankiit.maisuria33@gmail.com>
2026-06-08 06:41:59 -0700

    decision-making statements in Python

---
+++ 05_decision_making/mini_project_01.py
@@ -0,0 +1,9 @@
+name = "Ankiit"
+city = "Vancouver"
+
+if city == "Toronto" :
+    print(f"{name} likes {city}")
+elif city == "Vancouver" :
+    print(f"{name} likes {city}")
+else:
+    print(f"{name} likes {city}")
